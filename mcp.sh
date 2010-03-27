f4d0cdb09e94c109d8275b27ee3c9eac389e9ca8
yekm <yekm@h0me>
2010-03-27 18:03:00 +0400

    mcp: not existing dest dir

--- mcp.sh
+++ mcp.sh
@@ -4,6 +4,12 @@ find -L "$from" -iname "$what" -type f | while read f ; do
 	base=${f##$from}
 	base=${base##/}
 	dn=$(dirname "$base")
+	if [ ! -d "$to/$dn" ] ; then
+		# if destination folder do not exist...
+		[ -z $create ] && break # ...skip
+		# ...or create and proceed
+		mkdir "$to/$dn"
+	fi
 	[ -n $FAKE ] && echo -e "$f\n$to/$dn\n"
 	[ -z $FAKE ] && cp -v "$f" "$to/$dn"
 done
