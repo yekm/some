e14238293a3cfd9b9d652981422857c455a26378
yekm <yekm@h0me>
2010-09-19 22:11:21 +0400

    auto copy

--- pano.sh
+++ pano.sh
@@ -4,11 +4,12 @@ ls -1 $@ | while read f ; do
 	j="${f%%.*}"
 	echo "$0: $f ($j)"
 	convert "$f" -quality 95 "$j.jpg"
-	cp -v "$j.jpg" "/mnt/shome/ftp/p/pano/$P"
+	[ -n "$P" ] && cp -v "$j.jpg" "/mnt/shome/ftp/p/pano/$P"
 	f="$j.jpg"
 	echo "$0: $f"
 	[ ! -f "$f.tif" ] && \
 		vips im_vips2tiff "$f" "$f.tif":jpeg:95,tile:256x256,pyramid
-	cp -v "$f.tif" "/mnt/shome/ftp/p/pyramid/$P"
+	[ -n "$P" ] && mv -v "$f.tif" "/mnt/shome/ftp/p/pyramid/$P"
+	create=1 TODO=thumb from="/mnt/shome/ftp/p/pano" to="/mnt/shome/ftp/p/thumb" what="*.jpg" mcp.sh
 done
 
