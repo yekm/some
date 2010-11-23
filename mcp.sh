9822841f8893f2091b463cc463d5ef698a6bef8b
yekm <yekm@h0me>
2010-11-24 00:10:46 +0300

    sed lol

--- mcp.sh
+++ mcp.sh
@@ -19,7 +19,7 @@ find -L "$from" -iname "$what" -type f | while read f ; do
 		[ -z $FAKE ] && [ ! -f "$o2" ] && convert "$o1" -resize x20 "$o2"
 	;;
 	fat)
-		tofn=$(echo "$fn" | tr -d '\/:*?<>|')
+		tofn=$(echo "$fn" | sed -e 's/[^a-z0-9A-Z\-\(\)]/_/g')
 		#todn=$(echo "$dn" | tr -d '\/:*?<>|')
 		echo -e "$f \t\t $to/$dn/$tofn"
 		[ -z $FAKE ] && cp -v "$f" "$to/$dn/$tofn"
