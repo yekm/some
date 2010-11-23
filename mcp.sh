8315173d958884c51adaeeeca2c4c211d8dd04a8
yekm <yekm@h0me>
2010-11-23 23:57:20 +0300

    fat todo

--- mcp.sh
+++ mcp.sh
@@ -11,15 +11,21 @@ find -L "$from" -iname "$what" -type f | while read f ; do
 		# ...or create and proceed
 		mkdir -p "$to/$dn"
 	fi
-	[ -n $FAKE ] && echo -e "$f ---> $to/$dn/$fn"
-	case "$TODO" in
+	case "$todo" in
 	thumb)
 		o1="$to/$dn/$fn"
 		o2="$to/$dn/small_$fn"
 		[ -z $FAKE ] && [ ! -f "$o1" ] && convert "$f" -resize x400 "$o1"
 		[ -z $FAKE ] && [ ! -f "$o2" ] && convert "$o1" -resize x20 "$o2"
 	;;
+	fat)
+		tofn=$(echo "$fn" | tr -d '\/:*?<>|')
+		#todn=$(echo "$dn" | tr -d '\/:*?<>|')
+		echo -e "$f \t\t $to/$dn/$tofn"
+		[ -z $FAKE ] && cp -v "$f" "$to/$dn/$tofn"
+	;;
 	*)
+		echo -e "$f \t\t $to/$dn"
 		[ -z $FAKE ] && cp -v "$f" "$to/$dn"
 	;;
 	esac
