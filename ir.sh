8d9a9942fa95a74093168d67fbc60bb71cb6e317
yekm <yekm@h0me>
2012-03-31 12:58:22 +0400

    mpd

--- ir.sh
+++ ir.sh
@@ -2,117 +2,109 @@
 
 cmd="$1"
 
-c=1
-amixer -c 0 controls | grep "PCM Playback Volume" && c=0
+c=2
+#amixer -c 2 controls | grep "PCM Playback Volume" && c=0
 
-log=~/some/log.ir.sh
+log=/home/yekm/log.ir.sh
 #echo "`date` $cmd" >>$log
 export DISPLAY=:0
+mpc='mpc -h yekm_mpd@localhost -p 16600'
 
 case "$cmd" in
 play)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action play_or_pause
+		mplayer -send-action play_or_pause
 	else
-		#qdbus org.kde.amarok /Player PlayPause
-		mocp --toggle-pause
+		$mpc toggle
 	fi
 ;;
 stop)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action fullscreen
+		mplayer -send-action fullscreen
 	else
-		#qdbus org.kde.amarok /Player Stop
-		mocp --stop
+		$mpc --stop
 	fi
 ;;
 bwd)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action play_prev
+		mplayer -send-action play_prev
 	else
-		#qdbus org.kde.amarok /Player Prev
-#		qdbus org.kde.amarok /Player ShowOSD
-		mocp --previous
+		$mpc prev
 	fi
 ;;
 fwd)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action play_next
+		mplayer -send-action play_next
 	else
-		#qdbus org.kde.amarok /Player Next
-#		qdbus org.kde.amarok /Player ShowOSD
-		mocp --next
+		$mpc next
 	fi
 ;;
 repeat)
 ;;
 
 b1)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action rewind1
+		mplayer -send-action rewind1
 	else
-		#qdbus org.kde.amarok /Player Backward 2
-		mocp --seek -2
+		$mpc seek -00:00:02
 	fi
 ;;
 b2)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action forward1
+		mplayer -send-action forward1
 	else
 		#qdbus org.kde.amarok /Player Forward 2
-		mocp --seek 2
+		$mpc seek +00:00:02
 	fi
 ;;
 b3)
 ;;
 b4)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action rewind1
+		mplayer -send-action rewind1
 	else
-		#qdbus org.kde.amarok /Player Backward 20
-		mocp --seek -20
+		$mpc seek -00:00:20
 	fi
 ;;
 b5)
-	if pgrep smplayer
+	if pgrep mplayer
 	then
-		smplayer -send-action forward1
+		mplayer -send-action forward1
 	else
-		#qdbus org.kde.amarok /Player Forward 20
-		mocp --seek 20
+		$mpc seek +00:00:20
 	fi
 ;;
 b6)
 	killall mplayer
 ;;
 b7)
-	smplayer -send-action dec_zoom
+	mplayer -send-action dec_zoom
 ;;
 b8)
-	smplayer -send-action inc_zoom
+	mplayer -send-action inc_zoom
 ;;
 b9)
-	smplayer -send-action reset_zoom
+	mplayer -send-action reset_zoom
 ;;
 b10)
-	smplayer -send-action dec_sub_scale
+	mplayer -send-action dec_sub_scale
 ;;
 b+10)
-	smplayer -send-action inc_sub_scale
+	mplayer -send-action inc_sub_scale
 ;;
 
 vol+)
-	amixer -c $c set PCM 2dB+
+	amixer -c $c set Master 1dB+
 ;;
 vol-)
-	amixer -c $c set PCM 2dB-
+	amixer -c $c set Master 1dB-
 ;;
 esac
 
