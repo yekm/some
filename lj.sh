46e576957f63cbb6f190d298da5ab4d97772b97f
yekm <source@yekm.name>
2013-04-12 04:39:53 +0400

    lj.sh update

--- lj.sh
+++ lj.sh
@@ -3,11 +3,6 @@
 [ -z "$1" ] && echo "specify username" && exit -1
 ljuser=$1
 ljhost="$ljuser.livejournal.com"
-years=$(wget -O - "$ljhost/calendar" | perl -ne 'm,'$ljhost'/(\d{4})/, && print "$1\n"' | sort -u)
-for year in $years; do
-    days=$(wget -O - "$ljhost/$year" | perl -ne 'm,'$ljhost'/(\d{4})/(\d{2})/(\d{2}), && print "$1/$2/$3\n"' | sort -u)
-    for day in $days; do
-        echo "$ljhost/$day"
-    done
-done | sort -u | tee $ljuser-posts
-
+years=$(wget -O - "$ljhost/calendar" | perl -ne 'm,'$ljhost'/(\d{4})/, && print "'$ljhost/'$1\n"' | sort -u)
+days=$(wget -O - $years | perl -ne 'm,'$ljhost'/(\d{4})/(\d{2})/(\d{2}), && print "'$ljhost/'$1/$2/$3\n"' | sort -u)
+wget -O - $days | perl -ne 'm,'$ljhost'/(\d+).html, && print "'$ljhost'/$1.html\n"' | sort -u | tee $ljuser-posts
